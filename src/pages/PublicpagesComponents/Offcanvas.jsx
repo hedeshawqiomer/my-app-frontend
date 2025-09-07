243f2b2a2e9c15eab342ec4b7d5c2e44fc3a8bb0
hedeshawqiomer <realhede7@gmail.com>
2025-09-07 16:26:35 +0300

    protect5

--- src/pages/PublicpagesComponents/Offcanvas.jsx
+++ src/pages/PublicpagesComponents/Offcanvas.jsx
@@ -1,6 +1,6 @@
 
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import {  useNavigate } from "react-router-dom";
 
 function Offcanvas() {
   const navigate = useNavigate();
