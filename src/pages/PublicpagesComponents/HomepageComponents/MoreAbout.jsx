86c23ab07ab0ce78771affe47022ae1caba98a97
hedeshawqiomer <realhede7@gmail.com>
2025-12-21 19:37:30 +0300

    update project1

--- src/pages/PublicpagesComponents/HomepageComponents/MoreAbout.jsx
+++ src/pages/PublicpagesComponents/HomepageComponents/MoreAbout.jsx
@@ -1,47 +1,46 @@
 import React from "react";
 
+function MoreAbout() {
+  return (
+    <section className="secondsec2" id="heroSection">
+      <div className="container">
+        <div className="row flex-lg-row-reverse align-items-center justify-content-between g-5 py-5">
+          <div className="col-10 col-sm-12 col-lg-6 image-col mx-auto d-flex justify-content-center">
+            <img
+              src="/pictures/map2.png"
+              className="img-fluid custom-hero-img slide-down"
+              alt="..."
+              loading="lazy"
+            />
+          </div>
 
-function MoreAbout(){
-return(
-      
-      <section className="secondsec2" id="heroSection">
-  <div className="container">
-    <div className="row flex-lg-row-reverse align-items-center justify-content-between g-5 py-5">
+          <div className="col-lg-6">
+            <h1
+              className="display-5 fw-bold lh-1 mb-4 fade-down"
+              style={{
+                fontFamily: "sans-serif",
+                background: "linear-gradient(to right, #d8cb16, #6d6603)",
+                WebkitBackgroundClip: "text",
+                WebkitTextFillColor: "transparent",
+                backgroundClip: "text",
+                color: "transparent",
+              }}
+            >
+              Discover the Perfect Spot for Your Next Vacation
+            </h1>
 
-      <div className="col-10 col-sm-12 col-lg-6 image-col mx-auto d-flex justify-content-center">
-        <img 
-          src="/pictures/map2.png" 
-          className="img-fluid custom-hero-img slide-down" 
-          alt="..." 
-          loading="lazy" 
-        />
+            <p className="lead fw-bold fade-down">
+              Planning a picnic starts with finding the right location a place
+              that combines natural beauty, comfort, and accessibility. Ideal
+              picnic spots often include parks, riverbanks, lakesides, or quiet
+              hills with scenic views. When searching for the perfect place, you
+              can also check online maps, reviews, and local recommendations to
+              discover hidden gems
+            </p>
+          </div>
+        </div>
       </div>
-
-      <div className="col-lg-6">
-        <h1 
-          className="display-5 fw-bold lh-1 mb-4 fade-down" 
-          style={{
-            fontFamily: "sans-serif",
-            background: "linear-gradient(to right, #d8cb16, #6d6603)",
-            WebkitBackgroundClip: "text",
-            WebkitTextFillColor: "transparent",
-            backgroundClip: "text",
-            color: "transparent"
-          }}
-        >
-          Discover the Perfect Spot for Your Next Vacation
-        </h1>
-
-        <p className="lead fw-bold fade-down">
-          Planning a picnic starts with finding the right location a place that combines natural beauty, comfort, and accessibility. Ideal picnic spots often include parks, riverbanks, lakesides, or quiet hills with scenic views. When searching for the perfect place, you can also check online maps, reviews, and local recommendations to discover hidden gems.
-        </p>
-      </div>
-
-    </div>
-  </div>
-</section>
-      
-)
-
+    </section>
+  );
 }
-export default  MoreAbout
+export default MoreAbout;
