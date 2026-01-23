c2ef60d6cb4c742913a6bf8f51e3e587f57a58d8
hedeshawqiomer <realhede7@gmail.com>
2026-01-23 18:36:57 +0300

    just refreshing

--- src/ErrorBondry.jsx
+++ src/ErrorBondry.jsx
@@ -4,7 +4,7 @@ export default class ErrorBoundary extends React.Component {
   constructor(props){ super(props); this.state={hasError:false}; }
   static getDerivedStateFromError(){ return {hasError:true}; }
   render(){
-    if(this.state.hasError) return <div className="text-center py-5">Something went wrong.</div>;
+    if(this.state.hasError) return <div className="text-center py-5">Something went wrong</div>;
     return this.props.children;
   }
 }
