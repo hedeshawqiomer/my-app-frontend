41172229aee2876bc0d9547ab182b0c559bad7c5
hedeshawqiomer <realhede7@gmail.com>
2026-02-20 22:58:00 +0300

    city center fix

--- src/utills/CityCenter.js
+++ src/utills/CityCenter.js
@@ -1,10 +1,10 @@
 // src/utills/cityCenters.js
 // src/utills/CityCenter.js
 const cityCenters = {
-  Erbil:        { lat: 36.2000, lng: 44.0000 },
-  Sulaimani: { lat: 35.5610, lng: 45.4420 }, // approx city center
-  Duhok:        { lat: 36.0000, lng: 43.0000 }, // approx city center
-  Halabja:      { lat: 35.1833, lng: 45.9833 },
+  Erbil:        { lat: 36.1884981, lng: 43.9866016 },
+  Sulaimani: { lat: 35.5609323, lng: 45.4105702 }, // approx city center
+  Duhok:        { lat: 36.862587, lng: 42.9950694 }, // approx city center
+  Halabja:      { lat: 35.1860539, lng: 45.9554586 },
 };
 
 
